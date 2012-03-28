f44a6f5bedcb0fca871862753277be00890a9998
Tim Baker <timbone945@gmail.com>
2012-03-28 11:46:04 -0400

    removed models

--- controllers/evaluation.php
+++ controllers/evaluation.php
@@ -3,3 +3,24 @@
 $app->get('/evaluation', function() use ($app) {
 	echo "Evaluation";
 });
+
+
+$app->get('/evaluation', function() use ($app) {
+	echo "Evaluation";
+});
+
+$app->get('/evaluation', function() use ($app) {
+	echo "Evaluation";
+});
+
+$app->get('/evaluation', function() use ($app) {
+	echo "Evaluation";
+});
+
+$app->get('/evaluation', function() use ($app) {
+	echo "Evaluation";
+});
+
+$app->get('/evaluation', function() use ($app) {
+	echo "Evaluation";
+});
